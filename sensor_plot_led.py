63ed540421aea9a3143cc9d3a877e8165ae73d0b
chibaf <chibaf@mac.com>
2024-10-17 11:45:58 +0900

    Add files via upload

---
+++ sensor_plot_led.py
@@ -0,0 +1,22 @@
+import serial, sys
+import matplotlib.pyplot as plt
+
+x=range(0, 100, 1)
+data=[0.0]*100
+
+ser=serial.Serial(sys.argv[1],sys.argv[2])
+print("connected to: " + ser.portstr)
+while True:
+  line = ser.readline()
+  line1=line.strip().decode('utf-8')
+#  if line1=='':
+#    line1="0.0"
+  line3=line1.split(',')
+  d=float(line3[1])
+  print(d)
+  data.pop(-1)
+  data.insert(0,d)
+  plt.clf()
+  plt.ylim([-1.0,1500.0])
+  plt.plot(x,data)
+  plt.pause(0.1)
